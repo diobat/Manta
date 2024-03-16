2a59e7120b29f2aef00cbbfa27d0f8e52c4db00c
Diogo Batista <diogobatista@ua.pt>
2024-03-16 15:20:13 +0100

    Spatial components

---
+++ src/ECS/components/spatial.cpp
@@ -0,0 +1,35 @@
+#include "ECS/components/spatial.hpp"
+
+void setPosition(entt::registry& registry, entt::entity& entity, const glm::vec3& value)
+{
+    auto& pos = registry.get<position>(entity);
+    pos.value = value;
+}
+
+void deltaPosition(entt::registry& registry, entt::entity& entity, const glm::vec3& value)
+{
+    auto& pos = registry.get<position>(entity);
+    pos.value += value;
+}
+
+const glm::vec3& getPosition(entt::registry& registry, entt::entity& entity)
+{
+    return registry.get<position>(entity).value;
+}
+
+void setRotation(entt::registry& registry, entt::entity& entity, const glm::vec3& value)
+{
+    auto& rot = registry.get<rotation>(entity);
+    rot.value = value;
+}
+
+void deltaRotation(entt::registry& registry, entt::entity& entity, const glm::vec3& value)
+{
+    auto& rot = registry.get<rotation>(entity);
+    rot.value += value;
+}
+
+const glm::vec3& getRotation(entt::registry& registry, entt::entity& entity)
+{
+    return registry.get<rotation>(entity).value;
+}
---
+++ src/ECS/components/spatial.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <entt.hpp>
+#include "wrapper/glm.hpp"
+
+struct position
+{
+    glm::vec3 value;
+};
+
+void setPosition(entt::registry& registry, entt::entity& entity, const glm::vec3& value);
+void deltaPosition(entt::registry& registry, entt::entity& entity, const glm::vec3& value);
+const glm::vec3& getPosition(entt::registry& registry, entt::entity& entity);
+
+
+struct rotation
+{
+    glm::vec3 value;
+};
+
+void setRotation(entt::registry& registry, entt::entity& entity, const glm::vec3& value);
+void deltaRotation(entt::registry& registry, entt::entity& entity, const glm::vec3& value);
+const glm::vec3& getRotation(entt::registry& registry, entt::entity& entity);
