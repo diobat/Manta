af9205abfa2b765db21a5a50d518f1442f986842
Diogo Batista <diogobatista@ua.pt>
2024-05-14 20:37:32 +0200

    HDR image import support

--- src/util/imageData.cpp
+++ src/util/imageData.cpp
@@ -1,18 +1,28 @@
 #include "util/imageData.hpp"
 
+#include "helpers/RootDir.hpp"
+
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
-loadedImageData STB_load_image(const std::string& path)
+loadedImageDataRGB STB_load_image(const std::string& path)
 {
-    loadedImageData img;
+    loadedImageDataRGB img;
     img.data = stbi_load(path.c_str(), &img.width, &img.height, &img.channels, STBI_rgb_alpha);
     return img;
 };
 
-loadedImageData ASSIMP_load_image(const aiTexture* texture)
+loadedImageDataHDR STB_load_image_HDR(const std::string &path)
 {
-    loadedImageData img;
+    loadedImageDataHDR img;
+    std::string fullPath = ROOT_DIR + path;
+    img.data = stbi_loadf(fullPath.c_str(), &img.width, &img.height, &img.channels, STBI_rgb_alpha);
+    return img;
+}
+
+loadedImageDataRGB ASSIMP_load_image(const aiTexture* texture)
+{
+    loadedImageDataRGB img;
     img.data = stbi_load_from_memory(
         reinterpret_cast<const unsigned char*>(texture->pcData),
         texture->mWidth,
@@ -24,6 +34,11 @@ loadedImageData ASSIMP_load_image(const aiTexture* texture)
 };
 
 void free_image(unsigned char* data)
+{
+    stbi_image_free(data);
+}
+
+void free_image(float* data)
 {
     stbi_image_free(data);
 }
--- src/util/imageData.hpp
+++ src/util/imageData.hpp
@@ -3,16 +3,24 @@
 #include <string>
 #include <assimp/scene.h>
 
+template<typename T>
 struct loadedImageData
 {
-    unsigned char* data;
+    T* data;
     int width;
     int height;
     int channels;
 };
 
-loadedImageData STB_load_image(const std::string& path);
+using loadedImageDataRGB = loadedImageData<unsigned char>;
+using loadedImageDataHDR = loadedImageData<float>;
 
-loadedImageData ASSIMP_load_image(const aiTexture* texture);
+loadedImageDataRGB STB_load_image(const std::string& path);
+
+loadedImageDataHDR STB_load_image_HDR(const std::string &path);
+
+loadedImageDataRGB ASSIMP_load_image(const aiTexture* texture);
 
 void free_image(unsigned char* data);
+
+void free_image(float* data);
